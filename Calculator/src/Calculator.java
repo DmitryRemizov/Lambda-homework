e1d7dc93a731b2aabbff616451ef9f3d2de345c4
Dmitry Remizov <rdk.7777@yahoo.com>
2023-04-06 13:14:54 +0300

    first commit

---
+++ Calculator/src/Calculator.java
@@ -0,0 +1,24 @@
+import java.util.function.*;
+
+public class Calculator {
+    static Supplier<Calculator> instance = Calculator::new;
+
+    BinaryOperator<Integer> plus = (x, y) -> x + y;
+    BinaryOperator<Integer> minus = (x, y) -> x - y;
+    BinaryOperator<Integer> multiply = (x, y) -> x * y;
+    BinaryOperator<Integer> devide = (x, y) ->
+    {
+        if (y == 0) {
+            System.out.println("На ноль делить нельзя");
+            return Integer.MAX_VALUE;
+        }
+        return x / y;
+    };
+
+    UnaryOperator<Integer> pow = x -> x * x;
+    UnaryOperator<Integer> abs = x -> (x > 0) ? x : -x;
+
+    Predicate<Integer> isPositive = x -> x > 0;
+
+    Consumer<Integer> println = System.out::println;
+}
